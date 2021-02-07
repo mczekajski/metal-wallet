4e4df40927246812feb55c2e7f1de0ee427d50b0
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-02-07 15:19:14 +0100

    Use scss, set responsive background image

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'metal-wallet'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('metal-wallet');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'metal-wallet';
+export class AppComponent implements OnInit {
+  canvas: any;
+  ctx: any;
+  constructor() {}
+  ngOnInit(): void {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    const myChart = new Chart(this.ctx, {
+      type: 'doughnut',
+      data: {
+        labels: [
+          'Gold',
+          'Silver'
+        ],
+        datasets: [
+          {
+            label: 'Total cases.',
+            data: [
+              2,
+              10
+            ],
+            backgroundColor: ['gold', '#ccc'],
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+        responsive: false,
+        display: true,
+      },
+    });
+  }
 }
