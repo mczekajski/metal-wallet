aa4ba8cb733a72fcd80cb6c91e9e2cc387bd23bb
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-03-22 20:47:41 +0100

    Add json data

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const material = [
+  MatButtonModule,
+  MatToolbarModule
+]
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [material],
+  exports: [material]
 })
 export class MaterialModule { }
